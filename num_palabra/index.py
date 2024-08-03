81b00f607d73e74b6a3962a782c1f265fbc199b1
Matias Bensan Araya <mbensan@gmail.com>
2024-08-02 20:36:08 -0400

    NUM Palabras completado

--- num_palabra/index.py
+++ num_palabra/index.py
@@ -1,6 +1,15 @@
 import sys
 import math
 
+def secuencia(num_elementos):
+  lista = [0, 1]
+  for i in range(2, num_elementos + 1):
+    if i % 2 == 0:
+      lista.append(lista[i-1] + lista[i-2])
+    else:
+      lista.append(lista[i-1] * 2)
+  return lista
+
 def num_palabra(num):
 
   primeros15 = ['cero', 'uno', 'dos', 'tres', 'cuatro', 'cinco',
@@ -47,6 +56,29 @@ def num_palabra(num):
     if resto == 0:
       return centenas_palabras[centenas]
     return centenas_palabras[centenas] + ' ' + num_palabra(resto)
+  elif num == 1000:
+    return 'luca'
+  elif num <= 1999: # 1523
+    resto = num - 1000
+    return 'mil ' + num_palabra(resto)
+  # 755_600 - 23_592 - 10_000
+  elif num <= 999999:
+    miles = math.floor(num / 1000)
+    resto = num - (miles * 1000)
+    if resto == 0:
+      return num_palabra(miles) + ' mil'
+    return num_palabra(miles) + ' mil ' + num_palabra(resto)
+  elif num == 1000000:
+    return 'un millón'
+  elif num <= 1999999:
+    resto = num - 1000000
+    return 'un millón ' + num_palabra(resto)
+  elif num < 1_000_000_000_000:
+    millones = math.floor(num / 1_000_000)
+    resto = num - (millones * 1_000_000)
+
+    return num_palabra(millones) + ' millones ' + num_palabra(resto)
+
   else: 
     return 'no implementado'
 
